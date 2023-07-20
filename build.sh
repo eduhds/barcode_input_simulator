83fbc4a69aba74f7d32d31f37a5029cecf07d273
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2023-07-20 08:20:10 -0300

    fix: build script

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ cp __main__.py dist/__main__.py
 
 pipenv run pip install -r <(pipenv requirements) --target dist
 
-if [ "$1" = "standalone" ]; then
-    interpreter="-p /usr/bin/python"
-else
-    interpreter=""
-fi
+out=barcode_input_simulator.pyz
 
-python -m zipapp dist -c -o barcode_input_simulator.pyz $interpreter
+if [ "$1" = "standalone" ]; then
+    python -m zipapp dist -c -o "$out" -p "/usr/bin/env python"
+else
+    python -m zipapp dist -c -o  "$out"
+fi
